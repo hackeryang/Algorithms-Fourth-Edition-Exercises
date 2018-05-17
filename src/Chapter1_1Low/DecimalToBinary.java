605fc1b5c1a182162841384f5e71efa5c7a6c9b3
hackeryang <375446972@qq.com>
2018-05-17 15:29:25 +0800

    add annotations to exercise 1.1.9

--- src/Chapter1_1Low/DecimalToBinary.java
+++ src/Chapter1_1Low/DecimalToBinary.java
@@ -6,7 +6,7 @@ public class DecimalToBinary {
     public static String decimalToBinary(int n){
         String resultString="";
         for(int i=31;i>=0;i--)
-            resultString=resultString+(n>>>i&1);
+            resultString=resultString+(n>>>i&1);  //将整数每一个高位右移至相应的最低位再“与”1，结果转换为字符串从左到右排列输出，依然是原来的二进制
         return resultString;
     }
     public static void main(String[] args){
